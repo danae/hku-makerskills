84af67b899ec94a2a4712c09be0e5bcfb52ccced
dengsn <info@dengsn.com>
2018-03-26 14:45:56 +0200

    Updated weather script

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
-import spidev
 import time
+from Adafruit_GPIO.SPI import SpiDev as SPI
+from Adafruit_MCP3008 import MCP3008
 from Adafruit_CharLCD import Adafruit_CharLCD as LCD
 
 # Weather device class
@@ -13,29 +14,27 @@ class WeatherDevice:
   lcd_d6 = 23
   lcd_d7 = 18
 
+  # Constants for the SPI connection
+  spi_port = 0
+  spi_device = 0
+
   # Other constants
   lcd_columns = 16
   lcd_rows = 2
 
   # Constructor
   def __init__(self):
-    # Create the SPI input
-    self.spi = spidev.SpiDev()
-    self.spi.open(0,0)
+    # Create the MCP input
+    self.mcp = MCP3008(spi=SPI(self.spi_port,self.spi_device))
 
     # Create the LCD output
     self.lcd = LCD(self.lcd_rs,self.lcd_en,self.lcd_d4,self.lcd_d5,self.lcd_d6,self.lcd_d7,self.lcd_columns,self.lcd_rows)
 
-  # Read a channel from the SPI device
-  def read_spi(self, channel):
-    spidata = self.spi.xfer2([1,(8+channel)<<4,0])
-    return ((spidata[1] & 3) << 8) + spidata[2]
-
   # Poll the current internal temperature
   def poll_internal_temperature(self):
-    channeldata = self.read_spi(0)
-    voltage = round(channeldata * (5.0 / 1024.0),0)
-    return (voltage - 500) / 10
+    channeldata = self.mcp.read_adc(0)
+    volts = channeldata * (5.0 / 1024.0)
+    return (volts - 0.5) * 100.0
   
   # Poll the current external temperature
   def poll_external_temperature(self, query = "Utrecht, NL"):
@@ -51,50 +50,46 @@ def main():
   device = WeatherDevice()
   device.lcd.create_char(0,[12,18,18,12,0,0,0,0])
 
-  # Create a timer
+  # Create a timer and a state for the LCD
   timer = 0
   state = -1
 
-  try:
-    # Main loop
-    while True:
-      # Poll data every half minute
-      if timer % 30 == 0:
-        # Get the temperatures
-        location = "Kerkrade, NL"
-        int_temp = device.poll_internal_temperature()
-        ext_temp = device.poll_external_temperature(location)
+  # Variables for data storage
+  location = "Utrecht, NL"
+  int_temp = 0
+  ext_temp = 0
 
-        # Print to the console
-        print(" Fetched data!")
-        print("- Location: {}".format(location))
-        print("- Internal temperature: {} °C".format(int_temp))
-        print("- Outside temperature: {} °C".format(ext_temp))
-        print()
+  # Main loop
+  while True:
+    # Poll API data every 30 seconds
+    if timer % 30 == 0:
+      ext_temp = device.poll_external_temperature(location)
 
-      # Change the lcd state every 5 seconds
-      if timer % 5 == 0:
-        # Change the state
-        state = (state + 1) % 2
+    # Change state every 5 seconds
+    if timer % 5 == 0:
+      state = (state + 1) % 2
 
-        # Clear the display
-        device.lcd.clear()
+    # Pol lthe internal temperature
+    int_temp = device.poll_internal_temperature()
 
-        # Print according to the state
-        if state == 0:
-            device.lcd.message("{0:^16}\n{1:^16}".format(time.strftime("%H:%M"),time.strftime("%d-%m-%Y")))
-        elif state == 1:
-          # Print temperatures
-          device.lcd.message("Ext:  {0:>7.1f} {2}C\nInt:  {1:>7.1f} {2}C".format(ext_temp,int_temp,chr(0)))
+    # Clear the display
+    #device.lcd.clear()
+    device.lcd.home()
 
-      # Increase the timer
-      timer += 1
+    # Print according to the state
+    if state == 0:
+      # Print date and time
+      device.lcd.message("{0:^16}\n{1:^16}".format(time.strftime("%H:%M:%S"),time.strftime("%d-%m-%Y")))
+    elif state == 1:
+      # Print temperatures
+      device.lcd.message("Ext:  {0:>7.1f} {2}C\nInt:  {1:>7.1f} {2}C".format(ext_temp,int_temp,chr(0)))
 
-      # Sleep for some time
-      time.sleep(1.0)
+    # Increase the timer
+    timer += 1
 
-  except KeyboardInterrupt:
-    device.spi.close()
+    # Sleep for some time
+    time.sleep(1.0)
+
 
 # Execute the main function
 if __name__ == '__main__':
